d68239e5c5a9614be833fb99ade61acf0c2d1d9e
sylvilagus <matraton@sina.com>
2016-03-24 16:39:38 +0800

    fix some bug and optimize some logic

--- ViewDragHelperResearch/app/src/main/java/com/sylva/vdhr/widget/DraggableLayout.kt
+++ ViewDragHelperResearch/app/src/main/java/com/sylva/vdhr/widget/DraggableLayout.kt
@@ -16,11 +16,7 @@ import java.util.HashMap
 class DraggableLayout : RelativeLayout {
     private var mViewDragHelper: ViewDragHelper? = null
     private val mChildrenProperties = HashMap<Int, ChildProperty>()
-    fun Int.between(e1:Int,e2:Int)=Math.min(e1,e2)<this&&this<Math.max(e1,e2)
-    fun Any.le()= Log.e("DraggableLayout",this.toString())
-    private interface OperationToChildren {
-        fun operate(id: Int?, childProperty: ChildProperty)
-    }
+    fun Any.le(prefix:String=":")= Log.e("DraggableLayout",prefix+this.toString())
 
     private val mCallback = object : ViewDragHelper.Callback() {
         override fun tryCaptureView(child: View, pointerId: Int): Boolean {
@@ -32,12 +28,21 @@ class DraggableLayout : RelativeLayout {
             val childProperty=mChildrenProperties[child?.id]
             var leftFinal:Int
             childProperty?:throw IllegalStateException()
+            childProperty.currentX=left
             var intersectResult:Int=0
             val barriers=mChildrenProperties.map{it.value}.
                     filter {
-                        intersectResult=childProperty.intersectX(it)
-                        it!=childProperty&&it.type!= ChildProperty.Type.INTANGIBLE&&intersectResult>0
+                        if(it==childProperty)
+                            false
+                        else {
+                            val ir = childProperty.intersectX(it)
+                            if (ir > 0)
+                                intersectResult = ir
+                            it.type != ChildProperty.Type.INTANGIBLE && ir > 0
+                        }
                     }
+            barriers.size.le("barriersX.size")
+            intersectResult.le("intersectXResult:")
 
             leftFinal= barriers.map {
                 when(intersectResult){
@@ -56,21 +61,17 @@ class DraggableLayout : RelativeLayout {
             val childProperty=mChildrenProperties[child?.id]
             var topFinal:Int
             childProperty?:throw IllegalStateException()
-            var intersectResult:Int=0
-            val barriers=mChildrenProperties.map{it.value}.
-                    filter {
-                        intersectResult=childProperty.intersectY(it)
-                        it!=childProperty&&it.type!= ChildProperty.Type.INTANGIBLE&&intersectResult>0
-                    }
-
-            topFinal= barriers.map {
-                when(intersectResult){
-                    ChildProperty.INTERSECT_LT->return it.currentY-childProperty.height-1
-                    ChildProperty.SUPERPOSE->return it.currentY-childProperty.height-1
-                    ChildProperty.INTERSECT_GT->return it.currentY+it.height+1
-                    else->top
-                }
-            }.min()?:top
+            childProperty.currentY=top
+            topFinal=mChildrenProperties.map{it.value to childProperty.intersectY(it.value)}.
+                    filter {it.first!=childProperty&&it.first.type != ChildProperty.Type.INTANGIBLE && it.second>0}.
+                    map {
+                        when(it.second){
+                            ChildProperty.INTERSECT_LT->return it.first.currentY-childProperty.height-1
+                            ChildProperty.SUPERPOSE->return it.first.currentY-childProperty.height-1
+                            ChildProperty.INTERSECT_GT->return it.first.currentY+it.first.height+1
+                            else->top
+                        }
+                    }.min()?:top
 
             childProperty.currentY=topFinal
             return topFinal
@@ -81,6 +82,7 @@ class DraggableLayout : RelativeLayout {
             if (cp!=null&&cp.type == ChildProperty.Type.ELASTIC) {
                 mViewDragHelper!!.settleCapturedViewAt(cp.originalX, cp.originalY)
                 invalidate()
+                cp.restore()
             }
         }
 
@@ -125,7 +127,7 @@ class DraggableLayout : RelativeLayout {
         super.onLayout(changed, l, t, r, b)
         for (i in 0..childCount - 1) {
             val child = getChildAt(i)
-            mChildrenProperties.put(child.id, ChildProperty.fromChild(child, if (i % 2 == 0) ChildProperty.Type.ELASTIC else ChildProperty.Type.TANGIBLE))
+            mChildrenProperties.put(child.id, ChildProperty.fromChild(child, ChildProperty.Type.TANGIBLE))
         }
     }
 
@@ -171,28 +173,32 @@ class DraggableLayout : RelativeLayout {
             }
         }
         fun intersectX(cp:ChildProperty):Int{
-            if(currentY<cp.currentY-height||currentY>cp.currentY+cp.height)
+            if(currentY<=cp.currentY-height||currentY>=cp.currentY+cp.height)
                 return SEPARATE
-            if(currentX>cp.currentX&&currentX<cp.currentX+cp.width)
+            if(currentX>=cp.currentX&&currentX<=cp.currentX+cp.width)
                 return INTERSECT_GT
             else if(currentX==cp.currentX)
                 return SUPERPOSE
-            else if(currentX>cp.currentX-width&&currentX<cp.currentX)
+            else if(currentX>=cp.currentX-width&&currentX<=cp.currentX)
                 return INTERSECT_LT
             else
                 return SEPARATE
         }
         fun intersectY(cp:ChildProperty):Int{
-            if(currentX<cp.currentX-width||currentX>cp.currentX+cp.width)
+            if(currentX<=cp.currentX-width||currentX>=cp.currentX+cp.width)
                 return SEPARATE
-            if(currentY>cp.currentY&&currentY<cp.currentY+cp.height)
+            if(currentY>=cp.currentY&&currentY<=cp.currentY+cp.height)
                 return INTERSECT_GT
             else if(currentY==cp.currentY)
                 return SUPERPOSE
-            else if(currentY>cp.currentY-height&&currentY<cp.currentY)
+            else if(currentY>=cp.currentY-height&&currentY<=cp.currentY)
                 return INTERSECT_LT
             else
                 return SEPARATE
+        }
+        fun restore(){
+            currentX=originalX
+            currentY=originalY
         }
     }
 }
